b744ba3bf5ecba114845379ecf431404e892971d
JSE955 <jordanscottedwards@gmail.com>
2024-08-07 20:27:32 -0400

    Add function to count characters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
         file_contents = f.read()
 
     print(count_words(file_contents))
+    print(count_characters(file_contents))
 
 def count_words(text):
     words = text.split()
@@ -10,6 +11,16 @@ def count_words(text):
     for word in words:
         count += 1
     return count
+
+def count_characters(text):
+    result = {}
+    for chr in text:
+        lower_chr = chr.lower()
+        if lower_chr in result:
+            result[lower_chr] += 1
+        else:
+            result[lower_chr] = 1
+    return result
 
 main()
 
